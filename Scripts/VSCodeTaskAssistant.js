71e9918657819b24e32cec8c11ddb5642fafaa6d
Elliot <1073323+kurucu@users.noreply.github.com>
2023-03-06 07:42:05 +0000

    Changed type checking to null safe operator

--- Scripts/VSCodeTaskAssistant.js
+++ Scripts/VSCodeTaskAssistant.js
@@ -1,55 +1,65 @@
 module.exports.VSCodeTaskAssistant = class VSCodeTaskAssistant {
   constructor() {
-	this.packageProcessName = "code";
-	this.packageJsonPath = nova.workspace.path + "/.vscode/tasks.json";
+    this.packageProcessName = "code";
+    this.packageJsonPath = nova.workspace.path + "/.vscode/tasks.json";
   }
 
   provideTasks() {
-	let tasks = [];
-	let tasksFile = nova.fs.stat(this.packageJsonPath);
+    let tasks = [];
+    let tasksFile = nova.fs.stat(this.packageJsonPath);
 
-	/*
-	 * tasks.json
-	 */
-	if (tasksFile && tasksFile.isFile()) {
-	  try {
-		const commentRegex = /^( *\/\/.*\n)/gm;
-		const trailingCommaRegex = /\,(?!\s*?[\{\[\"\'\w])/gm;
-		let fileContents = nova.fs.open(this.packageJsonPath).read().replace(commentRegex, "").replace(trailingCommaRegex, "");
-		let pack = JSON.parse(fileContents);
-		
-		pack.tasks.forEach((vsTask) => {
-			if (vsTask.command === undefined) { return }
-			
-			let task = new Task(vsTask.label)
-			
-			let args = vsTask.command.split(' ');
-			let command = args.shift();
-			
-			let cwd = nova.workspace.path
-			// Creating error: ReferenceError: Can't find variable: options
-			// if (typeof vsTask.options != 'undefined' && typeof vsTask.options.cwd != 'undefined') {
-			// 	cwd = options.cwd.replace("workspaceFolder", "WorkspaceFolder").replace("fileDirname", "FileDirname")
-			// }
-			
-			task.setAction(
-				// Creating error TypeError: undefined is not an object (evaluating 'vsTask.group.kind')
-				vsTask.group == "build" || (typeof vsTask.group.kind != 'undefined' && vsTask.group.kind == "build") ? Task.Build : Task.Run, 
-				new TaskProcessAction(command, {
-					args: args,
-					shell: true,
-					cwd: cwd
-				})
-			);
-		
-			tasks.push(task)
-		})
-	  } catch (e) {
-		console.info("error");
-		console.log(e);
-	  }
-	}
+    /*
+     * tasks.json
+     */
+    if (tasksFile && tasksFile.isFile()) {
+      try {
+        const commentRegex = /^( *\/\/.*\n)/gm;
+        const trailingCommaRegex = /\,(?!\s*?[\{\[\"\'\w])/gm;
+        let fileContents = nova.fs
+          .open(this.packageJsonPath)
+          .read()
+          .replace(commentRegex, "")
+          .replace(trailingCommaRegex, "");
+        let pack = JSON.parse(fileContents);
 
-	return tasks;
+        pack.tasks.forEach((vsTask) => {
+          if (vsTask.command === undefined) {
+            return;
+          }
+
+          let task = new Task(vsTask.label);
+
+          let args = vsTask.command.split(" ");
+          let command = args.shift();
+
+          let cwd = nova.workspace.path;
+          // Creating error: ReferenceError: Can't find variable: options
+          if (typeof vsTask.options?.cwd != "undefined") {
+            cwd = vsTask.options.cwd
+              .replace("workspaceFolder", "WorkspaceFolder")
+              .replace("fileDirname", "FileDirname");
+          }
+
+          task.setAction(
+            // Creating error TypeError: undefined is not an object (evaluating 'vsTask.group.kind')
+            vsTask?.group == "build" || vsTask?.group?.kind == "build"
+              ? Task.Build
+              : Task.Run,
+            new TaskProcessAction(command, {
+              args: args,
+              shell: true,
+              cwd: cwd,
+            })
+          );
+
+          tasks.push(task);
+        });
+      } catch (e) {
+        console.info("error");
+        console.log(e);
+      }
+    }
+
+    return tasks;
   }
 };
