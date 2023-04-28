bb0b1026f22976327bdf2bd7e1ba4612a1ebab63
theMackabu <theMackabu@gmail.com>
2023-04-27 22:50:06 -0700

    fix taskfile duplication

--- src/parsers/taskfile.ts
+++ src/parsers/taskfile.ts
@@ -10,6 +10,7 @@ class Taskfile {
 	constructor() {
 		this.packageProcessName = 'task';
 		this.tasks = [];
+		this.iter = 0;
 		this.options = {
 			cwd: nova.workspace.path as string,
 			args: ['--list-all'],
@@ -41,6 +42,14 @@ class Taskfile {
 			taskfile.onStderr((line) => console.warn(`finder (taskfile) extraction error: ${line}`));
 			const onExit = new Promise((resolve, reject) => {
 				taskfile.onDidExit((status) => {
+					/* hacky fix to prevent duplication of tasks
+					 * issue: all tasks seem to run twice when
+					 * the watcher reloads the task.
+					 */
+					if (this.iter == 0) {
+						this.tasks = [];
+					}
+
 					console.log(`exited: finder (taskfile) with code ${status}`);
 					const action = status == 0 ? resolve : reject;
 					action(status);
@@ -55,6 +64,8 @@ class Taskfile {
 	}
 
 	async provideTasks() {
+		this.tasks = [];
+		this.iter = 0;
 		await this.findTasks();
 		console.info(`taskfile has ${this.tasks.length} task(s)`);
 		return this.tasks;
